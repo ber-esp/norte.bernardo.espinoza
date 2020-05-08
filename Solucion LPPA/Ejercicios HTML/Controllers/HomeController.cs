cefdf26bbd867ec1f7ec896ce9d7f2a0440cad63
BERNARDO ESPINOZA <b-j-espinoza@hotmail.com>
2020-05-07 23:31:47 -0300

    Completo.

---
+++ Solucion LPPA/Ejercicios HTML/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicios_HTML.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioCB1()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioCB2()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioCB3()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioCB4()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioEnlaces()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioListas()
+        {
+            return View();
+        }
+        public ActionResult EjercicioTablas1()
+        {
+            return View();
+        }
+        public ActionResult EjercicioTablas2()
+        {
+            return View();
+        }
+        public ActionResult EjercicioFormularios1()
+        {
+            return View();
+        }
+        public ActionResult EjercicioFormularios2()
+        {
+            return View();
+        }
+        public ActionResult EjercicioFormularioshtml5()
+        {
+            return View();
+        }
+    }
+}
---
+++ Solucion LPPA/Ejercicios HTML/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicios_HTML.MvcApplication" Language="C#" %>
