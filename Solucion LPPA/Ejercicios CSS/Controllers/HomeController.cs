a12d63c2cfce3908c61cf268456c7087d5b1314a
BERNARDO ESPINOZA <b-j-espinoza@hotmail.com>
2020-05-12 00:09:31 -0300

    Ejercicios CSS.

---
+++ Solucion LPPA/Ejercicios CSS/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicios_CSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EJ1()
+        {
+            return View();
+        }
+        public ActionResult EJ2()
+        {
+            return View();
+        }
+        public ActionResult EJ3()
+        {
+            return View();
+        }
+        public ActionResult EJ4()
+        {
+            return View();
+        }
+        public ActionResult EJ5()
+        {
+            return View();
+        }
+        public ActionResult EJ6()
+        {
+            return View();
+        }
+        public ActionResult EJ7()
+        {
+            return View();
+        }
+        public ActionResult EJ8()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ Solucion LPPA/Ejercicios CSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicios_CSS.MvcApplication" Language="C#" %>
